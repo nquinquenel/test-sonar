2c00afa65ee4ebf1f5104cff1c0c3a088b5a7e87
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-03-22 16:13:45 +0100

    Init project

---
+++ test-sonar-java/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+  int myPow(int num, int exponent) {
+    num = num * myPow(num, exponent - 1);
+    return num;  // this is never reached
+  }
+
+}
