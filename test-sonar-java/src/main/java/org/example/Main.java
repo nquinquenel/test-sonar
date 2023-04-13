f6625c2bddc5c5e849957134167942b64eeb5a1f
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-04-13 15:01:31 +0200

    Issue

--- test-sonar-java/src/main/java/org/example/Main.java
+++ test-sonar-java/src/main/java/org/example/Main.java
@@ -1,16 +1,40 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        ExtractEntry(null);
     }
 
+        static private String targetDirectory = "/example/directory/";
+
+    public static void ExtractEntry(ZipFile zipFile) throws IOException {
+
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        ZipEntry entry = entries.nextElement();
+        InputStream inputStream = zipFile.getInputStream(entry);
+
+        File file = new File(targetDirectory + entry.getName());
+
+        Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
 
     int myPow(int num, int exponent) throws ClassNotFoundException {
         num = num * myPow(num, exponent - 1);
 
         String className = System.getProperty("Main");
         Class clazz = Class.forName(className);  // Noncompliant
+
+
 
         return num;  // this is never reached
     }
