71a99363f1a34524c8f187ec270cdade78827d63
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-04-13 14:55:01 +0200

    Issue

--- test-sonar-java/src/main/java/org/example/Main.java
+++ test-sonar-java/src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 
 
-  int myPow(int num, int exponent) {
-    num = num * myPow(num, exponent - 1);
-    return num;  // this is never reached
-  }
+    int myPow(int num, int exponent) {
+        num = num * myPow(num, exponent - 1);
 
-  int myPows(int num, int exponent) {
-    num = num * myPow(num, exponent - 1);
-    return num;  // this is never reached
-  }
+        String className = System.getProperty("Main");
+        Class clazz = Class.forName(className);  // Noncompliant
+
+        return num;  // this is never reached
+    }
+
+    int myPows(int num, int exponent) {
+        num = num * myPow(num, exponent - 1);
+        return num;  // this is never reached
+    }
 
 }
