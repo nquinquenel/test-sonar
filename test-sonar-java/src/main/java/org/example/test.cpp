490052fa09c46b36972d397ff9c505d7fbde2792
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-07-13 10:24:05 +0200

    Test

---
+++ test-sonar-java/src/main/java/org/example/test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by nicolas.quinquenel on 23/06/23.
+//
+
+#include "test.h"
+
+struct Task {
+    struct promise_type {
+        /* ... */
+        void unhandled_exception() {}
+    };
+};
