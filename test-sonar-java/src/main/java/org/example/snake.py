36c2f839f27ec297ba73b2852415c95019bb269e
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-08-02 17:16:32 +0200

    Test

--- test-sonar-java/src/main/java/org/example/snake.py
+++ test-sonar-java/src/main/java/org/example/snake.py
@@ -1,9 +1,1 @@
-class A:
-    def __mul__(self, other, unexpected):  # Noncompliant. Too many parameters
-        return 42
-
-    def __add__(self):  # Noncompliant. Missing one parameter
-        return 42
-
-A() * 3  # TypeError: __mul__() missing 1 required positional argument: 'unexpected'
-A() + 3  # TypeError: __add__() takes 1 positional argument but 2 were given
+A()
