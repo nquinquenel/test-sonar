b62cbe0b11292c9d0b0b39d45d83222cd8f3d092
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-11-20 09:57:17 +0100

    test

--- test-sonar-java/src/main/java/org/example/MyClass.java
+++ test-sonar-java/src/main/java/org/example/MyClass.java
@@ -1,6 +1,13 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class MyClass implements Enumeration {
   @Override
@@ -13,4 +20,21 @@ public class MyClass implements Enumeration {
     return null;
   }  // Noncompliant
   /* ... */
+
+  static private String targetDirectory = "/example/directory/";
+
+  public static void ExtractEntry5(ZipFile zipFile) throws IOException {
+    var toto = "tedst";
+
+    Enumeration<? extends ZipEntry> entries = zipFile.entries();
+    Enumeration<? extends ZipEntry> entries2 = zipFile.entries();
+    Enumeration<? extends ZipEntry> entries3 = zipFile.entries();
+
+    ZipEntry entry = entries.nextElement();
+    InputStream inputStream = zipFile.getInputStream(entry);
+
+    File file = new File(targetDirectory + entry.getName());
+
+    Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+  }
 }
