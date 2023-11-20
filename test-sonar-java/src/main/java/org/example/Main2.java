27390aa774b72325592b4c65da8a52947d23bcb2
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-11-20 09:20:01 +0100

    new taint

--- test-sonar-java/src/main/java/org/example/Main2.java
+++ test-sonar-java/src/main/java/org/example/Main2.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -24,5 +29,22 @@ public class Main2 {
 
   public boolean foo() {
     return false;
+  }
+
+  static private String targetDirectory = "/example/directory/";
+
+  public static void ExtractEntry(ZipFile zipFile) throws IOException {
+    var toto = "tedst";
+
+    Enumeration<? extends ZipEntry> entries = zipFile.entries();
+    Enumeration<? extends ZipEntry> entries2 = zipFile.entries();
+    Enumeration<? extends ZipEntry> entries3 = zipFile.entries();
+
+    ZipEntry entry = entries.nextElement();
+    InputStream inputStream = zipFile.getInputStream(entry);
+
+    File file = new File(targetDirectory + entry.getName());
+
+    Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
   }
 }
